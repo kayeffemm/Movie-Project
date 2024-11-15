48d5781f017831d8a069ce1f40a22cd83a1f58a7
kayeffemm <kenneth.meier95@gmail.com>
2024-11-15 15:56:10 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from classes.movie_app import MovieApp
 from classes.storage_json import StorageJson
+from classes.storage_csv import StorageCSV
 
-storage = StorageJson("data/movie_database.json")
-movie_app = MovieApp(storage)
 
-movie_app.run()
+HTML_TEMPLATE_PATH = 'static/index_template.html'
+HTML_OUTPUT_PATH = 'static/index.html'
+
+
+def main():
+    """
+    Instantiate IStorage object, MovieApp object and call the run method from MovieApp object.
+    """
+    storage_json = StorageJson('data/movie_database.json')
+    movie_app_json = MovieApp(storage_json, HTML_TEMPLATE_PATH, HTML_OUTPUT_PATH)
+
+    storage_csv = StorageCSV('data/movie_database.CSV')
+    movie_app_csv = MovieApp(storage_csv, HTML_TEMPLATE_PATH, HTML_OUTPUT_PATH)
+
+    # Choose one of them. Command line argument to start coming soon.
+    #movie_app_json.run()
+    movie_app_csv.run()
+
+
+if __name__ == "__main__":
+    main()
